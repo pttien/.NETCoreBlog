c6f54ddb20dce96555e43c40a7bc162785137c3d
Tien Pham <tien.phanthanh@nashtechglobal.com>
2018-10-19 15:49:22 +0700

    change

--- BlogDemo/BlogDemo.Web/Migrations/20181019084755_addCreateDatetoComment.Designer.cs
+++ BlogDemo/BlogDemo.Web/Migrations/20181019084755_addCreateDatetoComment.Designer.cs
@@ -4,14 +4,16 @@ using BlogDemo.Domain.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogDemo.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181019084755_addCreateDatetoComment")]
+    partial class addCreateDatetoComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
