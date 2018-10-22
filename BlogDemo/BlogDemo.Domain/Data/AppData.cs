79ae205ab459d3163e650f3ded28357187d6c4e8
Tien Pham <tien.phanthanh@nashtechglobal.com>
2018-10-22 12:50:03 +0700

    changes

--- BlogDemo/BlogDemo.Domain/Data/AppData.cs
+++ BlogDemo/BlogDemo.Domain/Data/AppData.cs
@@ -30,6 +30,7 @@ namespace BlogDemo.Domain.Data
                 Email = "member@us.com",
                 DisplayName = "Demo user",
                 Bio = "Short description about this user and blog.",
+                Avatar = "data/member/Avatar.jpg",
                 Created = DateTime.UtcNow.AddDays(-110)
             });
 
@@ -68,14 +69,15 @@ namespace BlogDemo.Domain.Data
 
             context.Posts.Add(new Post
             {
-                Title = "Demo post",
-                Slug = "demo-post",
+                Title = "Fabletics x Girl Up Meet the Girl Up leaders from Fabletics new collection!",
+                Slug = "fabletics-x-girl-up-meet-the-girl-up-leaders-from-fabletics-new-collection",
                 Description = "This demo site is a sandbox to test Blogifier features. It runs in-memory and does not save any data, so you can try everything without making any mess. Have fun!",
                 Content = SeedData.PostDemo,
                 AuthorId = demoId,
-                Cover = "data/demo/demo-cover.jpg",
+                Cover = "data/member/2018/10/Fabletics-2-Sized.png",
                 PostViews = 25,
                 Rating = 3.5,
+                Categories="Girl",
                 Published = DateTime.UtcNow.AddDays(-10)
             });
 
@@ -181,11 +183,42 @@ after clicking Next button, I would see the following dialog, ensure to check Un
 
 ";
 
-        public static readonly string PostDemo = @"This demo site is a sandbox to test Blogifier features. It runs in-memory and does not save any data, so you can try everything without making any mess. Have fun!
+        public static readonly string PostDemo = @"
+![data/member/2018/10/Fabletics-1-Sized.png](/data/member/2018/10/Fabletics-1-Sized.png)
 
-#### To login:
-* User: demo
-* Pswd: Demo@pass1";
+
+
+Fabletics’ new #GirlAlmighty capsule collection benefiting Girl Up,  is inspired by Kate Hudson’s vision for Fabletics to be a fashion-focused activewear brand with the mission to empower women by making a healthy, active lifestyle accessible to everyone. The new collection inspires girls and women to stand up and celebrate each other regardless of size, shape, age or ability.
+
+Members of Girl Up’s CSU Northridge campus chapter joined Kate Hudson for a #GirlAlmighty photo shoot featuring the collection in Rancho Palos Verdes, California – where the girls were able to model the fun styles. “I’m so excited to unveil the Girl Almighty collection in support of Girl Up. It has been a truly rewarding experience to work closely with the organization and the girls to create a collection that spreads the message that girls and women are the fearless leaders of tomorrow,” said co-founder of Fabletics, Kate Hudson.
+
+We talked to the Girl Up girl leaders who took part in the shoot to hear their thoughts on the new collection!
+
+![data/member/2018/10/Fabletics-2-Sized.png](/data/member/2018/10/Fabletics-2-Sized.png)
+
+Angelica Wong, 20 (right)
+
+Being able to work alongside Kate was such a blast; she was so friendly, humble and inviting– it was truly an incredible experience.
+
+I am so excited for the #GirlAlmighty line because it showcases the importance of strong girls supporting each other. Each piece from this line is so cute and trendy, all your girlfriends will want to join the movement. Girls can be cute AND strong and I feel like that’s what this line is really about which is awesome! Everything in this line truly does make you feel like you can take on the world, so I can’t wait to see more girls rocking the #GirlAlmighty line.
+
+The fact that this line supports body positivity and is truly celebrating girls standing up for each other means so much to me! Girl power starts within, if you feel good in the clothes you’re in, you’ll want to pass on that feeling and I feel like that is exactly what this line does. Face it, girls are strong and even stronger in numbers so we should focus on building each other up that way we can all reach our full potential. Plus, being able to do this all while wearing something that looks stunning and makes us feel amazing is awesome!
+
+Alondra Serrano, 22 (middle back)
+
+Kate was a great person all around! She was funny and made the shoot a very fun experience!
+
+I am excited about the #GirlAlmighty line because that phrase on its own is so powerful and says so much! As well as benefiting the great cause of Girl Up! Plus just like all of Fabletics lines, #GirlAlmighty is full of very stylish and comfortable pieces!
+
+#GirlAlmighty Fabletics line is so special! Promoting body positivity and overall girl empowerment! To me this is outstanding! Being a part of a sorority, I’ve learned that as girls we stand together and by doing so, you’ll always have endless support! When we work together we can become a voice for girls all around the world. It was a great honor to get to spread that through #GirlAlmighty!
+
+Michelle Reyes, 22 (left)
+
+Meeting Kate was the most amazing experience. She was so down to earth. Although I was initially star struck, she made me so comfortable around her.
+
+I am excited about the #GirlAlmighty line because I think it will remind women that we are powerful. We are the future. So many people will doubt us, and we may even doubt ourselves, but we can achieve anything and everything.
+
+To me, this means that we must remember that we are in this together. Women must unite in order to help others see that it is so important to just accept one and other for who we are. Individually unique, and together complete.";
 
     }
 }
